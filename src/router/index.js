657e5b5f295fd0ce6731550afaec9ab15631684e
罗成海 <1553650095@qq.com>
2019-09-18 21:19:51 +0800

    调整目录结构

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,18 @@
 import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import VueRouter from 'vue-router'
 
-Vue.use(Router)
+Vue.use(VueRouter)
 
-export default new Router({
+export default new VueRouter({
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
+      // path: '/',
+      // name: 'home',
+      // component: Home
     },
     {
-      path: '/about',
-      name: 'about',
+      // path: '/about',
+      // name: 'about',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
